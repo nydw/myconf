60cea4c447edebe5138727221e095d0e1f1e136c
nydw <nydw.lgx@qq.com>
2013-05-12 03:12:03 +0800

    修改：      c-setting.el

--- emacs.d/conf/my-lisps/c-setting.el
+++ emacs.d/conf/my-lisps/c-setting.el
@@ -1,18 +1,12 @@
-
 (require 'taglist)
 (require 'xcscope)
-(require 'cedet)
 (require 'which-func-setting)
 
 (setq c-default-style '((c-mode . "k&r") (other . "gnu")))    
 (setq c-basic-offset 4)    
 
 
-(setq semantic-default-submodes '(global-semantic-idle-scheduler-mode
-                                   global-semanticdb-minor-mode
-                                   global-semantic-idle-summary-mode
-                                   global-semantic-mru-bookmark-mode))
-(semantic-mode 1)
+
 
 
 
