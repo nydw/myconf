5ccf0d4c376f5493d215db9bb79f9669bff836a1
nydw <nydw.lgx@qq.com>
2013-05-12 03:16:11 +0800

    新文件：    key-map.el

---
+++ emacs.d/conf/my-lisps/key-map.el
@@ -0,0 +1,18 @@
+
+(global-set-key [(control c) (|)] 'toggle-window-split)
+(global-set-key (kbd "<C-f11>") 'toggle-line-spacing) 
+(global-set-key (kbd "M-f") 'forward-to-word)
+(global-set-key (kbd "M-b") 'backward-to-word)
+(global-set-key (kbd "C-M-=") 'increase-default-font-height)
+(global-set-key (kbd "C-M--") 'decrease-default-font-height)
+
+
+
+
+
+
+
+
+
+
+(provide 'key-map)
